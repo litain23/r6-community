58136670858314a8a54cfd2dcbd107b3529aa7a0
litain <litain23@gmail.com>
2020-08-03 18:04:41 +0900

    Exception 발생 시 print 추가

--- src/main/java/me/r6_search/config/GlobalExceptionHandler.java
+++ src/main/java/me/r6_search/config/GlobalExceptionHandler.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(BadCredentialsException.class)
     protected ResponseEntity<ErrorResponseDto> handleBadCredentialsException(BadCredentialsException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponse = new ErrorResponseDto();
         errorResponse.setMessage(e.getMessage());
         errorResponse.setStatus(401);
@@ -22,65 +22,73 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    protected  ResponseEntity<ErrorResponseDto> handleSignupException(IllegalArgumentException e) {
+    protected ResponseEntity<ErrorResponseDto> handleSignupException(IllegalArgumentException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto();
         errorResponseDto.setMessage(e.getMessage());
         errorResponseDto.setStatus(400);
-
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
-    }
-
+   }
 
     @ExceptionHandler(UserSignUpValidateException.class)
     protected ResponseEntity<ErrorResponseDto> handleUserValidateException(UserSignUpValidateException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserAuthenticationException.class)
     protected ResponseEntity<ErrorResponseDto> handlerUserAuthenticationException(UserAuthenticationException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CommentNotFoundException.class)
     protected ResponseEntity<ErrorResponseDto> handlerCommentNotFoundException(CommentNotFoundException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CommentIllegalModifyException.class)
     protected ResponseEntity<ErrorResponseDto> handlerCommentIllegalModifyException(CommentIllegalModifyException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(PostNotFoundException.class)
     protected ResponseEntity<ErrorResponseDto> handlerPostNotFoundException(PostNotFoundException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(PostIllegalModifyException.class)
     protected ResponseEntity<ErrorResponseDto> handlerPostIllegalModifyException(PostIllegalModifyException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(PostIllegalFileExtensionException.class)
     protected ResponseEntity<ErrorResponseDto> handlerPostIllegalFileExtensionException(PostIllegalFileExtensionException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BoardException.class)
     protected ResponseEntity<ErrorResponseDto> handlerBoardException(BoardException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
     protected ResponseEntity<ErrorResponseDto> handlerRuntimeException(RuntimeException e) {
+        e.printStackTrace();
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 400);
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
