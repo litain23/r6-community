1a8380d8747c9df83ed7f686ffd5538578c864a7
litain <litain23@gmail.com>
2020-08-12 14:44:40 +0900

    Cors 해결을 위한 Method 추가

--- src/main/java/me/r6_search/config/WebConfig.java
+++ src/main/java/me/r6_search/config/WebConfig.java
@@ -17,7 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+
     }
 
     @Override
